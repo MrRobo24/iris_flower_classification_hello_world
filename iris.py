a81768b4716f5e41653a16cac01f702923257013
Arpit Suman <sumanarpit.24@gmail.com>
2019-12-27 02:07:00 +0530

    Update iris.py

--- iris.py
+++ iris.py
@@ -13,3 +13,22 @@ import numpy as np
 iris = load_iris()
 
 X = iris.data
+y = iris.target
+
+y_names = iris.target_names
+
+test_ids = np.random.permutation(len(X))
+#keeping the last 10 etries for testing, rest for training
+
+X_train = X[test_ids[:-10]]
+X_test = X[test_ids[-10:]]
+y_train = y[test_ids[:-10]]
+y_test = y[test_ids[-10:]]
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(X_train,y_train)
+pred = clf.predict(X_test)
+
+print(pred)
+print(y_test)
+print(accuracy_score(pred,y_test)*100)
